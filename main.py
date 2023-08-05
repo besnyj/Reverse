a952fd9837d34dc584d7e7a708b473b0df11c5df
Felipe Nebesnyj <140928797+besnyj@users.noreply.github.com>
2023-08-05 06:46:42 -0300

    Posting the script

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+def reverse_lists(self):
+    output = []
+    for number in self: # for loop adds the object of the list before the given index in the output list, therefore changing the position of the first numbers with the lasts.
+        output.insert(0, number)
+    print(reversed_list)
+
+list_tobe_reversed = list(range(0, (random.randint(0, 10)))) #creates a random list.
+
+reverse_lists(list_tobe_reversed)
